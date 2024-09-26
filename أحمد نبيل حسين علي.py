fc1a75992998c2881d93b330c7b51a63253f6338
Ahmed7761-droid <ahmedalarasi185@gmail.com>
2024-09-26 17:18:39 +0300

    Add files via upload

---
+++ أحمد نبيل حسين علي.py
@@ -0,0 +1,23 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.5.33414.496
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{888888A0-9F3D-457C-B088-3A5042F75D52}") = "Mousa project", "Mousa project\Mousa project.pyproj", "{821A15F9-CA72-4AEF-B1C6-E808A284D398}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{821A15F9-CA72-4AEF-B1C6-E808A284D398}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{821A15F9-CA72-4AEF-B1C6-E808A284D398}.Release|Any CPU.ActiveCfg = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {3B147453-4ABC-4AC2-8207-F4F9CACE6C64}
+	EndGlobalSection
+EndGlobal
